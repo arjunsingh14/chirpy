7071308f1a5d67c76ba7a758c1331366ca588041
Arjun Bhandal <arjunsingh140601@gmail.com>
2026-07-15 23:06:42 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arjunsingh14/chirpy
+
+go 1.22.6
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app", http.StripPrefix("/app/", http.FileServer(http.Dir("./app"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	server := http.Server{Handler: mux, Addr: ":8080"}
+	server.ListenAndServe()
+
+}
